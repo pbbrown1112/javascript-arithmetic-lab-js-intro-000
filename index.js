ba70741e648e8ab9cec0d62ae72ea723c4cdca3c
Patrick B Brown <pbbrown1112@gmail.com>
2019-03-29 15:17:25 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,34 @@
+function add(a,b) {
+  return a + b;
+}
+
+function subtract(a,b) {
+  return a - b;
+}
+
+function multiply(a,b) {
+  return a * b;
+}
+
+function divide(a,b) {
+  return a / b;
+}
+
+function inc(a) {
+  return a+=1;
+}
+
+function dec(a) {
+  return a-=1;
+}
+
+function makeInt(a) {
+  {return parseInt(a.toString(),10);}
+  {return parseInt('0*238',10)}
+  {return parseInt('sldkjflksjf'.makeInt(),10)}
+}
+
+ function preserveDecimal(n) {
+   {return parseFloat('2.222')}
+   {return parseFloat('sldkjflksjf'.preserveDecimal())}
+ }
