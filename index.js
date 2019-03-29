27dc15fd2dcf27624e57214afc55f307c3c09793
Patrick B Brown <pbbrown1112@gmail.com>
2019-03-29 15:42:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function dec(a) {
   return a-=1;
 }
 
-function makeInt(a) {
+function makeInt(n) {
   {return parseInt(a.toString(),10);}
   {return parseInt('0*238',10)}
   {return parseInt('sldkjflksjf'.makeInt(),10)}
